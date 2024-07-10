96dca7ee386a58d9b64a35a3ba7f2dba44489190
Pirozoni <pirozoni@yandex.ru>
2024-07-10 17:00:11 +0300

    Исправления

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counter: Int = 0
+    @IBOutlet weak var tapCounter: UILabel!
+    @IBOutlet weak var tapButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tapCounter.text = "Значение счетчика: \(counter)"
     }
-
-
+    
+    @IBAction func PushButton(_ sender: Any) {
+        counter += 1
+        tapCounter.text = String("Значение счетчика: \(counter)")
+    }
 }
 
